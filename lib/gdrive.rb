4fbd00480421ea862ece6d4d3c71cca737672803
Badi Labassi <blabassi@aldogroup.com>
2014-04-07 16:15:24 -0400

    typo

--- lib/gdrive.rb
+++ lib/gdrive.rb
@@ -4,6 +4,5 @@ require 'gdrive/version'
 ::Middleman::Extensions.register(:gdrive) do
   require 'google_drive'
   require 'gdrive/extension'
-  require 'gdrive/helpers'
   ::Middleman::GDriveExtension
 end
