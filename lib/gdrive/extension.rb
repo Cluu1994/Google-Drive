dfdac8ed3f9a79f903d48ea5d7c67f254432060f
Badi Labassi <blabassi@aldogroup.com>
2015-04-17 12:22:49 -0400

    Now we can our files from everywhere

--- lib/gdrive/extension.rb
+++ lib/gdrive/extension.rb
@@ -1,5 +1,6 @@
 require 'fileutils'
 require 'oj'
+require 'pry'
 module Middleman
   class GDriveExtension < ::Middleman::Extension
     #include Middleman::CoreExtensions
@@ -33,11 +34,76 @@ module Middleman
         return session = gdauth.collection_by_title(banner).subcollection_by_title(season).subcollection_by_title(campaign)
       end
 
+      def split_path(path)
+        array = []
+        until ['/', '.'].include? path
+          array << File.basename(path)
+          path = File.dirname(path)
+        end
+        array.reverse
+      end
+
+      def fetch_path(source, dest)
+        source.subcollection_by_title(dest)
+      end
+
+      def getSheet(path, filename, worksheet)
+        # Create Google Authentication Session with Access Token
+        settings = YAML.load(File.open('data/credentials.yml'))
+        client = OAuth2::Client.new(
+            settings['google']['client_id'], settings['google']['client_secret'],
+            site: 'https://accounts.google.com',
+            token_url: '/o/oauth2/token',
+            authorize_url: '/o/oauth2/auth'
+        )
+        auth_token = OAuth2::AccessToken.from_hash( client, { refresh_token: settings['google']['refresh_token'] })
+        auth_token = auth_token.refresh!
+        auth = GoogleDrive.login_with_oauth(auth_token)
+        paths = split_path path
+        spreadsheet = filename.lstrip.rstrip
+        worksheet = worksheet.lstrip.rstrip
+        @banner_root = auth.collection_by_title(banner)
+        paths.each do |spreadsheet_path|
+          if @dir.nil?
+            # puts spreadsheet_path
+            @dir = fetch_path(@banner_root, spreadsheet_path)
+          else
+            # puts spreadsheet_path
+            @dir = fetch_path(@dir, spreadsheet_path)
+          end
+          # puts @dir
+        end
+        return Oj.load(@dir.file_by_title(spreadsheet).worksheet_by_title(worksheet).list.to_hash_array.to_json)
+        # cache_file = ::File.join('data/cache', "#{locale}_#{page}.json")
+        # time = Time.now
+        # session = session.subcollection_by_title(season).subcollection_by_title(campaign)
+        # if offline
+        #   puts "== You are currently viewing #{page} using the offline mode" unless build?
+        #   return page_data_request = Oj.load(::File.read(cache_file))
+        # end
+        # if !req.nil? && req.params['nocache'] || !req.nil? && req.GET.include?('nocache')
+        #   puts "== You are viewing #{page} directly from google drive"
+        #   return page_data_request = Oj.load(session.file_by_title(locale).worksheet_by_title(page).list.to_hash_array.to_json)
+        # elsif !req.nil? && req.params['refresh'] || !req.nil? && req.GET.include?('refresh')
+        #   puts "== Refreshing cache file for #{page}"
+        #   result = session.file_by_title(locale).worksheet_by_title(page).list.to_hash_array.to_json
+        #   ::File.open(cache_file, 'w')  { |f| f << result }
+        #   return page_data_request = Oj.load(::File.read(cache_file))
+        # else
+        #   if !::File.exist?(cache_file) || ::File.mtime(cache_file) < (time - cache_duration)
+        #     result = session.file_by_title(locale).worksheet_by_title(page).list.to_hash_array.to_json
+        #     ::File.open(cache_file, 'w')  { |f| f << result }
+        #   end
+        #   return page_data_request = Oj.load(::File.read(cache_file))
+        # end
+      end
+
       def gdrive(locale, page)
         locale = locale.lstrip.rstrip
         page = page.lstrip.rstrip
         cache_file = ::File.join('data/cache', "#{locale}_#{page}.json")
         time = Time.now
+        session = session.subcollection_by_title(season).subcollection_by_title(campaign)
         if offline
           puts "== You are currently viewing #{page} using the offline mode" unless build?
           return page_data_request = Oj.load(::File.read(cache_file))
